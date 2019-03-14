3f21e79e66ceec9714dbceb6d38c3ba356e6962e
Stephen Lane-Walsh <slwalsh@psfc.mit.edu>
2019-03-14 10:01:57 -0400

    Removed 3.3 from build-all.sh Locked 3.9-armhf base image down by version General cleanup

--- alpine/build-all.sh
+++ alpine/build-all.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-docker build -t mdsplus/docker:alpine-3.3-armhf -f Dockerfile.3.3-armhf . &
-docker build -t mdsplus/docker:alpine-3.3-x86 -f Dockerfile.3.3-x86 . &
-docker build -t mdsplus/docker:alpine-3.3-x86_64 -f Dockerfile.3.3-x86_64 . &
-docker build -t mdsplus/docker:alpine-3.9-armhf -f Dockerfile.3.3-armhf . &
-docker build -t mdsplus/docker:alpine-3.9-x86 -f Dockerfile.3.3-x86 . &
-docker build -t mdsplus/docker:alpine-3.9-x86_64 -f Dockerfile.3.3-x86_64 . &
+docker run --rm --privileged multiarch/qemu-user-static:register --reset
+
+docker build -t mdsplus/docker:alpine-3.9-armhf -f Dockerfile.3.9-armhf . &
+docker build -t mdsplus/docker:alpine-3.9-x86 -f Dockerfile.3.9-x86 . &
+docker build -t mdsplus/docker:alpine-3.9-x86_64 -f Dockerfile.3.9-x86_64 . &
