92cfec5305ab397f0f4d6acfc6e0d04aef0ab7a9
twf <twf@psfc.mit.edu>
2019-07-23 14:01:32 -0400

    Feature: reorganize services

--- jupyter/startup.py
+++
@@ -1,8 +0,0 @@
-# Show output of each python line separately
-from IPython.core.interactiveshell import InteractiveShell
-InteractiveShell.ast_node_interactivity = "all"
-# Load matplotlib to enable drawing plots of the data in the notebook
-import matplotlib.pyplot as plt
-# Enable the MDSplus magics %tcl and %tdi and import the MDSplus objects and functions
-import MDSplus.magic
-from MDSplus import *
