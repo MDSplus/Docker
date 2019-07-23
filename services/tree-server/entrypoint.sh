ec35bb368697f780ec4104e02f8d8638cb0dd9a9
twf <twf@psfc.mit.edu>
2019-07-23 14:00:51 -0400

    Feature: reorganize services

---
+++ services/tree-server/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+. /etc/profile.d/mdsplus.sh
+
+if [[ "x$default_tree_path" == "x" ]]; then
+    export default_tree_path="/trees/~t/"
+fi
+
+deluser hostuser >/dev/null 2>&1 || true
+delgroup hostgroup >/dev/null 2>&1 || true
+
+echo "Adding hostuser $UID:$GID"
+addgroup -g $GID hostgroup
+adduser -D -u $UID -G hostgroup hostuser
+
+inetd -fe
